9853db630e4d9d0371faec016e27723f0d976d68
InvisibleForest <grig.d.a@gmail.com>
2019-06-30 02:54:28 +0300

    add transition between main page and login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+link = 'http://selenium1py.pythonanywhere.com'
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -14,8 +14,9 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_guest_can_go_to_sign_in_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    login_page = LoginPage(browser, link)
-    login_page.open()
+def test_guest_can_go_to_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
