4467015fcc448484c97c9441f0e8509533aa6305
InvisibleForest <grig.d.a@gmail.com>
2019-07-03 01:09:06 +0300

    add login tests for product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,11 @@
 from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
 import pytest
 
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
+link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+'''@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -20,10 +21,21 @@ def  test_guest_can_add_product_to_cart(browser, link):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.product_name_is_the_same()
-    page.should_be_current_product_price()
+    page.should_be_current_product_price()'''
 
 
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page = BasePage(browser, browser.current_url)
+    page.go_to_login_page()
